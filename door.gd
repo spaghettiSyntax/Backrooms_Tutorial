73f5f2650083290c6259efa8a2391dc3618d4050
spaghettiSyntax <29992008+spaghettiSyntax@users.noreply.github.com>
2026-02-10 13:24:26 -0800

    Change door to extend StaticBody3D
    Refactor door to use StaticBody3D and update comments.

--- door.gd
+++ door.gd
@@ -1,19 +1,15 @@
-extends Node3D
-
+extends StaticBody3D
 
 @onready var hinge: Node3D = get_parent()
-
-
 var is_open = false
-
 
 func interact():
 	var tween = create_tween()
 	if is_open:
-		# Close the door
+		# Close it
 		tween.tween_property(hinge, "rotation:y", 0.0, 0.5)
 	else:
-		# Open the door (90 degrees)
+		# Open it (90 degrees)
 		tween.tween_property(hinge, "rotation:y", deg_to_rad(90), 0.5)
-		
+	
 	is_open = not is_open
