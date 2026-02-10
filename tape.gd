ddf317f2e9d265a4284bd8857c11d213f20ab701
spaghettiSyntax <29992008+spaghettiSyntax@users.noreply.github.com>
2026-02-10 13:27:49 -0800

    Enhance tape collection interaction
    Added prompt message for tape collection and improved interaction logic.

--- tape.gd
+++ tape.gd
@@ -1,7 +1,19 @@
 extends StaticBody3D
 
 
+@export var prompt_message = "Collect Tape"
+
+
 func interact():
-	print("Picked up Tape")
+	# Find the Player and update the count
+	var player = get_tree().get_first_node_in_group("Player")
+	
+	if player:
+		player.collect_tape()
+	
+	# Visual of the tape
+	visible = false
+	
 	# Play a sound here later
+	
 	queue_free()
